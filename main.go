9858ce6229b4ceb59034b35db650dfe3c9a4a463
Paul Lim <paulslim47@gmail.com>
2025-04-08 18:52:47 -0400

    hello world!

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+//package name 'main' used to make executable file from go build
+//other package names will not make executable file from go build
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!");
+}
