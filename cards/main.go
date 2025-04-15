855e22ca7bbadea2bc99217c82e86fcdd25c05b4
Paul Lim <paulslim47@gmail.com>
2025-04-15 18:50:28 -0400

    go

--- cards/main.go
+++ cards/main.go
@@ -2,7 +2,10 @@ package main
 
 import "fmt"
 
+// go is static type language like Java
+// go is able to infer the type of a variable
 func main() {
-	var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
 	fmt.Println(card)	
 }
