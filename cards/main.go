9d9f7dae16cae8866e272876e9ada933455ae8ee
Paul Lim <paulslim47@gmail.com>
2025-04-16 18:17:34 -0400

    function practice

--- cards/main.go
+++ cards/main.go
@@ -6,6 +6,13 @@ import "fmt"
 // go is able to infer the type of a variable
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
+	// colon = is only used for variable declaration
+	card := newCard()
+	// card = "8 of Hearts"
 	fmt.Println(card)	
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
